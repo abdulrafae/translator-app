55fe5f5f30a9c42ff4f53e0c0bc81d00576a4fae
abdulrafae <rafae015@gmail.com>
2024-07-14 18:09:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 #from transformer import AutoModelMT5ForConditionalGeneration, T5Tokenizer
-from transformer import pipeline
+from transformers import pipeline
 
 st.title("Stevens Translator")
 
